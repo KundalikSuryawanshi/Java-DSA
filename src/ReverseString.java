fa6d99329f361ec45223da6e1b5581436c0fa4ae
kundalik_suyawanshi <kundalik@atomx.in>
2023-12-26 23:43:47 +0530

    reverse-of-string-using-string-builder

---
+++ src/ReverseString.java
@@ -0,0 +1,31 @@
+
+public class ReverseString {
+    //reverse string using string builder
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("gaurav");
+
+        for (int i = 0; i < sb.length()/2; i++) {
+
+            int front = i;
+            int back = sb.length() -1 -i ; // 5 -1- 0 => 4
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+
+
+        }
+        System.out.println(sb);
+
+    }
+}
+
+/*
+* output
+* varuag
+* */
+
+
