582e7afb61b236b58e6452a02209188b044572d4
kundalik_suyawanshi <kundalik@atomx.in>
2023-12-27 00:00:28 +0530

    sha-generator-added'

--- src/SHAGenerator.kt
+++ src/SHAGenerator.kt
@@ -5,23 +5,23 @@ class SHAGenerator {
     //-- To generate 28 digit SHA key
     //import context
 
-    private fun printHashKey(context: Context) {
-
-        try {
-            val info = context.packageManager.getPackageInfo(
-                context.packageName,
-                PackageManager.GET_SIGNATURES
-            )
-            for (signature in info.signatures) {
-                val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                val hashKey = String(Base64.encode(md.digest(), 0))
-                Log.i("AppLog", "key:$hashKey=")
-            }
-        } catch (e: Exception) {
-            Log.e("AppLog", "error:", e)
-        }
-
-    }
+//    private fun printHashKey(context: Context) {
+//
+//        try {
+//            val info = context.packageManager.getPackageInfo(
+//                context.packageName,
+//                PackageManager.GET_SIGNATURES
+//            )
+//            for (signature in info.signatures) {
+//                val md = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                val hashKey = String(Base64.encode(md.digest(), 0))
+//                Log.i("AppLog", "key:$hashKey=")
+//            }
+//        } catch (e: Exception) {
+//            Log.e("AppLog", "error:", e)
+//        }
+//
+//    }
 
 }
